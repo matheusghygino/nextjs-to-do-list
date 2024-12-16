b34e1c45337223388af1e961ba7c6f268b48e391
Matheus Hygino <matheusghygino@gmail.com>
2024-12-16 19:36:00 -0300

    TodoItem completo

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+interface TodoItemProps {
+    todo: {
+        id: number;
+        text: string;
+        completed: boolean;
+    };
+    onDelete: (id: number) => void;
+    onToggle: (id: number) => void;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ( { todo, onDelete, onToggle } )  => {
+    return (
+        <div className="flex items-center justify-between p-4 border-b border-gray-200">
+            <div className="flex items-center">
+                <input 
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChangeCapture={() => onToggle(todo.id)}
+                    className="mr-4 form-checkbox h-5 w-5 text-blue-600"
+                />
+                <span
+                    className={`text-lg ${
+                        todo.completed ? 'line-through text-gray-400' : 'text-gray-800'
+                    }`}
+                >
+                    {todo.text}
+                </span>
+            </div>
+            <button
+                onClick={() => onDelete(todo.id)}
+                className="text-red-500 hover:text-red-700"
+            >
+                Excluir
+            </button>
+        </div>
+    );
+};
+
+export default TodoItem;
