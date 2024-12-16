d081369ec0de8e047536e2f9fe3c5022b1080d71
Matheus Hygino <matheusghygino@gmail.com>
2024-12-16 20:33:49 -0300

    finalizado to-do basico

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import TodoItem from "./TodoItem";
+import React from 'react';
+import TodoItem from './TodoItem';
 
 interface TodoListProps {
-    todos: {
-        id: number;
-        text: string;
-        completed: boolean;
-    }[];
-    onDelete: (id: number) => void;
-    onToggle: (id:number) => void;
+  todos: {
+    id: number;
+    text: string;
+    completed: boolean;
+  }[];
+  onDelete: (id: number) => void;
+  onToggle: (id: number) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = ({ todos, onDelete, onToggle }) => {
-    return (
-        <div className="bg-white shadow-md rounded-lg">
-            {todos.map((todo) => (
-                <TodoItem 
-                    key={todo.id}
-                    todo={todo}
-                    onDelete={onDelete}
-                    onToggle={onToggle}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className="bg-white shadow-md rounded-lg">
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
